11f764b08d49b470731ed3a34579504c922348ba
AgentSmalls <timbo2016@hotmail.com>
2020-11-28 16:15:05 -0700

    Small link updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,8 @@ import Header from "../components/header"
 export default function Home() {
     return (
         <div style={{ color: `purple` }}>
-            <Link to="/contact/">Contact</Link>
+            <Link to="./contact/">Contact</Link>
+			<Link to="./about/"> About</Link>
             <h1>Hello Gatsby!</h1>
             <p>What a world.</p>
             <img src="https://source.unsplash.com/random/400x200" alt="Random picture of something random" />
